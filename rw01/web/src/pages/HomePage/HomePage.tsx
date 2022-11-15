cd8df3244404385c260fcebf09bc210c0db73b8d
Karsten Gresch <git-2015@by-doing.org>
2022-11-15 12:35:35 +0100

    following tut yt

---
+++ rw01/web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,19 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+
+      <h1>Da Redwood Blok</h1>
+      <p>Welcome!</p>
+      <p>
+        My default route is named <code>home</code>, link to me with `
+        <Link to={routes.home()}>Home</Link>`
+      </p>
+    </>
+  )
+}
+
+export default HomePage
