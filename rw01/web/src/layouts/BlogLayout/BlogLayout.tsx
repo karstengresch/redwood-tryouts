fed600b88f96853595369f357ca5c80de2a3afb6
Karsten Gresch <git-2015@by-doing.org>
2022-11-16 17:10:24 +0100

    persistence

--- rw01/web/src/layouts/BlogLayout/BlogLayout.tsx
+++ rw01/web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -9,7 +9,7 @@ const BlogLayout = ({ children }: BlogLayoutProps) => {
     <>
       <header>
         <h1>
-          <Link to={routes.home()}>Da Blog</Link>
+          <Link to={routes.home()}>Redwood Blog</Link>
         </h1>
         <nav>
           <ul>
